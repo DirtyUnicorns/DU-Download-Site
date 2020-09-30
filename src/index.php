86d6fec83884e2f238dab77ca19472ccccc2a2c0
James Taylor <jmz.taylor16@gmail.com>
2020-09-29 20:29:50 -0500

    Remove unused parameter
    Change-Id: I6cd1f8b1f4b619af62cbc4cc100e616dfe6a4a0d

--- src/index.php
+++ src/index.php
@@ -49,7 +49,7 @@ Flight::route('/device/@device', function($device){
     ]);
 });
 
-Flight::route('/stats', function($device){
+Flight::route('/stats', function(){
     echo Flight::get("twig")->render('stats.html', [
         'stats' => getStats()
     ]);
